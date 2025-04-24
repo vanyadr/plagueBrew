77c913cdac6e2678afa6eb781a81df96d71d8df3
vanyadr <vanyadr@yandex.ru>
2025-04-24 14:03:41 +0300

    desktop about us section

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function horizontalScrollAbout() {
+        const myScrollY = window.scrollY;
+        const aboutOffset = document.querySelector('.about-wrapper').offsetTop;
+        const aboutHeight = document.querySelector('.about-wrapper').offsetHeight;
+        const myScrollWidth = document.querySelector('.about__holder').scrollWidth;
+        const viewportWidth = window.innerWidth
+        const myPadding = 80;
+        
+        if (myScrollY > aboutOffset && myScrollY < aboutOffset + myScrollWidth) {
+          const progress = (myScrollY - aboutOffset) / myScrollWidth;
+          const maxScroll = myScrollWidth - viewportWidth;
+          document.querySelector('.about__holder').style.transform = `translateX(-${progress * maxScroll}px)`;
+        }
+        if (myScrollY <= aboutOffset) {
+            document.querySelector('.about__holder').style.transform = `translateX(0)`;
+        }
+    }
+    
+    window.addEventListener('scroll', () => {
+        horizontalScrollAbout();
+    });
+});
